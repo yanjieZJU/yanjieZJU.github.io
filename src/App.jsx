46edc47286ec8dfc37afd5498a9ef95db76c1b69
yanjieZJU <yanjie02@zju.edu.cn>
2026-04-03 11:56:53 +0800

    feat: wire up HashRouter with home and project detail routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,35 @@
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import Education from './components/Education'
 import Projects from './components/Projects'
 import Skills from './components/Skills'
 import Footer from './components/Footer'
+import ProjectDetail from './components/ProjectDetail'
 import './App.css'
+
+function HomePage() {
+  return (
+    <>
+      <Hero />
+      <Education />
+      <Projects />
+      <Skills />
+    </>
+  )
+}
 
 export default function App() {
   return (
-    <>
+    <HashRouter>
       <Navbar />
       <main>
-        <Hero />
-        <div className="section-alt">
-          <Education />
-        </div>
-        <Projects />
-        <div className="section-alt">
-          <Skills />
-        </div>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/projects/:slug" element={<ProjectDetail />} />
+        </Routes>
       </main>
       <Footer />
-    </>
+    </HashRouter>
   )
 }
